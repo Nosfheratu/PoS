537bf11b041967ae3d37c599177c24c8e403c466
Fernando Serapio <fhernandosc@gmail.com>
2013-12-16 13:59:09 -0600

    Included SQLite, Simple.Data.SQLite and FluentMigrator
    Created PoS.Data, PoS.Entities and PoS.Services projects
    
    Moved entities from PoS/Entities to PoS.Entities
    
    updated README

---
+++ PoS.Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoS.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IList<Product> Products { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 PoS
 ===
+
+Migrate Database with:
+
+1) Compile PoS Solution
+2) Run Migrations with:
+	PM> Migrate.exe /conn "Data Source=PoS\Data\db.sqlite;Version=3;" /provider sqlite /assembly DLLs\PoS.Data.dll
