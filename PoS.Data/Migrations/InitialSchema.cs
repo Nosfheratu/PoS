cea2b2aaf98ec7f184f6ee3fa8e74c251e2cc02e
Fernando Serapio <fhernandosc@gmail.com>
2013-12-16 14:46:42 -0600

    Added Categories and Customers to database schema

--- PoS.Data/Migrations/InitialSchema.cs
+++ PoS.Data/Migrations/InitialSchema.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace PoS.Data.Migrations
 {
     [Migration(0001)]
-    public class CreateConfigurationsTable : Migration
+    public class InitialSchema : Migration
     {
         public override void Up()
         {
@@ -19,10 +19,24 @@ namespace PoS.Data.Migrations
                 .WithColumn("Phone").AsString(255).NotNullable()
                 .WithColumn("TicketsPrinter").AsString(255).NotNullable()
                 .WithColumn("ReportsPrinter").AsString(255).NotNullable();
+
+            Create.Table("Categories")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Name").AsString(255).NotNullable();
+
+            Create.Table("Customers")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("FullName").AsString(255).NotNullable()
+                .WithColumn("Address").AsString(255).NotNullable()
+                .WithColumn("State").AsString(255).NotNullable()
+                .WithColumn("City").AsString(255).NotNullable()
+                .WithColumn("ZipCode").AsString(255).NotNullable();
         }
 
         public override void Down()
         {
+            Delete.Table("Customers");
+            Delete.Table("Categories");
             Delete.Table("Configurations");
         }
     }
