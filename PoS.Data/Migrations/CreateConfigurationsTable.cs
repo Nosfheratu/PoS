e4da8ff43120df52e0179b10d4624727ed78c211
Fernando Serapio <fhernandosc@gmail.com>
2013-12-16 14:00:10 -0600

    Added a migration for 'Configurations' table

---
+++ PoS.Data/Migrations/CreateConfigurationsTable.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoS.Data.Migrations
+{
+    [Migration(0001)]
+    public class CreateConfigurationsTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Configurations")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("ShopName").AsString(255).NotNullable()
+                .WithColumn("Addess").AsString(255).NotNullable()
+                .WithColumn("Phone").AsString(255).NotNullable()
+                .WithColumn("TicketsPrinter").AsString(255).NotNullable()
+                .WithColumn("ReportsPrinter").AsString(255).NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Configurations");
+        }
+    }
+}
