7f9e35316f14c5e7766c3495fcc4c7978997a63c
Fernando Serapio <fhernandosc@gmail.com>
2013-12-17 00:01:21 -0600

    implemented UsersService, added Simple.Data references

---
+++ PoS.Services/UsersService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Simple.Data;
+using PoS.Entities;
+
+namespace PoS.Services
+{
+    public class UsersService
+    {
+        dynamic db;
+
+        public UsersService()
+        {
+            db = Database.OpenFile(GetDBPath());
+        }
+
+        private string GetDBPath()
+        {
+            return @"C:\Users\Fernando Serapio\Documents\visual studio 2012\Projects\PoS\DB\PoSDB.sqlite";
+        }
+
+        public List<User> GetAll()
+        {
+            return this.db.Users.All();
+        }
+
+        public bool Login(int userId, string password)
+        {
+            User user = db.Users.FindById(userId);
+
+            return user.ValidatePassword(password);
+        }
+    }
+}
