a66840774b9848e396dc5d847480edb84b295b23
Fernando Serapio <fhernandosc@gmail.com>
2013-12-19 16:33:37 -0600

    implemented Products service and repository

---
+++ PoS.Services/ProductsService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PoS.Entities;
+using PoS.Data.Repositories;
+
+namespace PoS.Services
+{
+    public class ProductsService
+    {
+        ProductsRepository productsRepository;
+        
+        public ProductsService()
+        {
+            productsRepository = new ProductsRepository();
+        }
+        
+        public Product Find(string barcode)
+        {
+            return productsRepository.GetAll().SingleOrDefault(p => p.Barcode == barcode);
+        }
+    }
+}
---
+++ PoS.Services/Repositories/ProductsRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Simple.Data;
+using PoS.Entities;
+
+namespace PoS.Data.Repositories
+{
+    public class ProductsRepository
+    {
+        dynamic db;
+
+        public ProductsRepository()
+        {
+            db = Database.OpenFile(GetDBPath());
+        }
+
+        private string GetDBPath()
+        {
+            return @"C:\Users\Fernando Serapio\Documents\visual studio 2012\Projects\PoS\DB\PoSDB.sqlite";
+        }
+
+        public List<Product> GetAll()
+        {
+            return db.Products.All();
+        }
+    }
+}
